6efc011f7d9e2143735b48f334a848f204476a1f
TimeEmit <LiamNorris1231853211@gmail.com>
2015-08-29 23:53:16 -0700

    Formulated first prediction!  0.75598 Accuracy

---
+++ predict/predict.py
@@ -0,0 +1,18 @@
+import pandas
+from learn.trainer import get_classifier
+from learn.trainer import A as winner
+ 
+DATA = pandas.read_csv('predict/input.csv')
+
+data = winner.data(DATA)
+classifier = get_classifier(winner)
+
+output = pandas.DataFrame({'PassengerId': DATA['passenger_id']})
+
+survived = list()
+for row in range(len(data)):
+    survival = classifier.predict(data.iloc[row ,:])
+    survived.append(survival[0])
+
+output['Survived'] = pandas.Series(survived)
+output.to_csv('predict/output.csv', index=False)
